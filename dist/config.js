8de52ebb26664a3d3e1029c8397391eee771781e
migreva <mikevee313@gmail.com>
2015-07-21 19:52:22 -0400

    removed dist

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  blogger: {
-    apiKey: 'AIzaSyAgMsEePa06vYnYJTWYfA_K746IaJ6HbC0'
-  }
-};
--- dist/routes/sockets.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var init = function init(app) {
-  app.io.route('blogger', function (req, res) {});
-};
-
-module.exports = {
-  init: init
-};
